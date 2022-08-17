4d248af3357000fd652fd071b94d803e25c6bc82
EvelynMH <91173236+evelynmh@users.noreply.github.com>
2022-08-16 23:39:03 -0500

    Add files via upload

---
+++ EF-ConfigServerPizza/src/test/java/com/idat/configserver/EfConfigServerPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfConfigServerPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
